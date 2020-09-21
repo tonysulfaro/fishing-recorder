2912d5390fb3da0594743c58bdcdcbb96e9f5a46
Tony Sulfaro <tony116523@gmail.com>
2020-09-21 16:39:12 -0400

    login figured out

--- client-app/src/Pages/LoginPage.jsx
+++ client-app/src/Pages/LoginPage.jsx
@@ -1,26 +1,31 @@
 import React from "react";
 import LoginButton from "../components/LoginButton";
+import { useAuth0 } from "@auth0/auth0-react";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-  useHistory,
-  useLocation,
-} from "react-router-dom";
+import { Link, Redirect, useHistory, useLocation } from "react-router-dom";
 
 const LoginPage = () => {
   let history = useHistory();
   let location = useLocation();
-
   let { from } = location.state || { from: { pathname: "/" } };
+  const { isAuthenticated } = useAuth0();
+
+  console.log(isAuthenticated);
+
+  if (isAuthenticated) {
+    return (
+      <Redirect
+        to={{
+          pathname: "/protected",
+          state: { from: location },
+        }}
+      />
+    );
+  }
 
   return (
     <div>
-      <h1>Login Page</h1>
-      <p>You must log in to view the page at {from.pathname}</p>
+      <h1>Login To Fishing Recorder</h1>
       <LoginButton></LoginButton>
     </div>
   );
