3b7f78c39ae259597df2ff5d4be4d0b0e3547a91
Tony Sulfaro <tony116523@gmail.com>
2020-09-21 19:00:41 -0400

    add sample markers

--- client-app/src/Pages/Protected.jsx
+++ client-app/src/Pages/Protected.jsx
@@ -1,8 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Protected.css";
-import LogoutButton from "../components/LogoutButton";
 import MapView from "../components/MapView";
-import Profile from "../components/Profile";
 import Navigation from "../components/Navigation";
 
 const Protected = () => {
@@ -10,7 +8,6 @@ const Protected = () => {
     <div className="App">
       <div>
         <Navigation></Navigation>
-        <h1>Protected</h1>
         <MapView></MapView>
       </div>
     </div>
--- client-app/src/components/MapView.jsx
+++ client-app/src/components/MapView.jsx
@@ -1,8 +1,76 @@
-import GoogleMapReact from "google-map-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./MapView.css";
+import GoogleMapReact from "google-map-react";
 
 const MapView = () => {
+  const [places, setplaces] = useState([]);
+
+  const getInfoWindowString = (place) => `
+    <div>
+      <div style="font-size: 16px;">
+        ${place.name}
+      </div>
+      <div style="font-size: 14px;">
+        <span style="color: grey;">
+        ${place.rating}
+        </span>
+        <span style="color: orange;">${String.fromCharCode(9733).repeat(
+          Math.floor(place.rating)
+        )}</span><span style="color: lightgrey;">${String.fromCharCode(
+    9733
+  ).repeat(5 - Math.floor(place.rating))}</span>
+      </div>
+      <div style="font-size: 14px; color: grey;">
+        ${place.types[0]}
+      </div>
+      <div style="font-size: 14px; color: grey;">
+        ${"$".repeat(place.price_level)}
+      </div>
+      <div style="font-size: 14px; color: green;">
+        ${place.opening_hours.open_now ? "Open" : "Closed"}
+      </div>
+    </div>`;
+
+  const handleApiLoaded = (map, maps, places) => {
+    const markers = [];
+    const infowindows = [];
+
+    places.forEach((place) => {
+      markers.push(
+        new maps.Marker({
+          position: {
+            lat: place.geometry.location.lat,
+            lng: place.geometry.location.lng,
+          },
+          map,
+        })
+      );
+
+      infowindows.push(
+        new maps.InfoWindow({
+          content: getInfoWindowString(place),
+        })
+      );
+    });
+
+    markers.forEach((marker, i) => {
+      marker.addListener("click", () => {
+        infowindows[i].open(map, marker);
+      });
+    });
+  };
+
+  useEffect(() => {
+    fetch("places.json")
+      .then((response) => response.json())
+      .then((data) => {
+        data.results.forEach((result) => {
+          result.show = false; // eslint-disable-line no-param-reassign
+        });
+        setplaces(data.results);
+      });
+  }, []);
+
   return (
     <div className="map-container">
       <GoogleMapReact
@@ -12,7 +80,11 @@ const MapView = () => {
           lng: -83.744,
         }}
         defaultZoom={7}
-      />
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={({ map, maps }) =>
+          handleApiLoaded(map, maps, places)
+        }
+      ></GoogleMapReact>
     </div>
   );
 };
