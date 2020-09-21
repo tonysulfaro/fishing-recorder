d090e7b5497ada024196a5507eced335a8b2c36a
Tony Sulfaro <tony116523@gmail.com>
2020-09-21 18:45:20 -0400

    make nav rresponsive

--- client-app/src/components/Navigation.jsx
+++ client-app/src/components/Navigation.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import "./Navigation.css";
-import { Navbar, Nav, Form, Button } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import LogoutButton from "./LogoutButton";
 
 const Navigation = (props) => {
   return (
-    <Navbar bg="dark" variant="dark" className="nav">
+    <Navbar
+      className="nav"
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+    >
       <Navbar.Brand href="#home">Fishing Recorder</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link active href="#home">
-          All Fish
-        </Nav.Link>
-        <Nav.Link href="#features">My Fish</Nav.Link>
-      </Nav>
-      <Form inline>
-        <LogoutButton></LogoutButton>
-      </Form>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#features">All Fish</Nav.Link>
+          <Nav.Link href="#pricing">My Fish</Nav.Link>
+        </Nav>
+        <Nav>
+          <LogoutButton></LogoutButton>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
