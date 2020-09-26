20217c394afc74052df8e89ba61ca71da70531aa
Tony Sulfaro <tony116523@gmail.com>
2020-09-25 21:42:04 -0400

    fit map to markers

--- client-app/src/components/MapView.jsx
+++ client-app/src/components/MapView.jsx
@@ -16,7 +16,33 @@ const MapView = (props) => {
       </div>
     </div>`;
 
+  // Return map bounds based on list of places
+  const getMapBounds = (map, maps, places) => {
+    const bounds = new maps.LatLngBounds();
+
+    places.forEach((place) => {
+      bounds.extend(new maps.LatLng(place.lat, place.lon));
+    });
+    return bounds;
+  };
+
+  // Re-center map when resizing the window
+  const bindResizeListener = (map, maps, bounds) => {
+    maps.event.addDomListenerOnce(map, "idle", () => {
+      maps.event.addDomListener(window, "resize", () => {
+        map.fitBounds(bounds);
+      });
+    });
+  };
+
   const handleApiLoaded = async (map, maps, places) => {
+    // Get bounds by our places
+    const bounds = getMapBounds(map, maps, places);
+    // Fit map to bounds
+    map.fitBounds(bounds);
+    // Bind the resize listener
+    bindResizeListener(map, maps, bounds);
+
     const markers = [];
     const infowindows = [];
 
